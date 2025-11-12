a9165e3a9859599fb6d246c2da60be79131b74c5
emeraudesantu <emeraudesantu05@mail.com>
2025-11-12 15:05:30 -0600

    etape 3

---
+++ lib/page_infos.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class PageInfos extends StatelessWidget {
+  const PageInfos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home : Scaffold(
+        appBar: AppBar(title: Text("PAge d'infos")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Bienvenu sur la page Infos !"),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
